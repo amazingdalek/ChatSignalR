31ec58734e317f7072826d653c33c1798b04654e
unknown <jordan.slusniak@hotmail.fr>
2019-10-16 20:35:50 +0200

    first commit

---
+++ Client_SignalR/obj/Debug/90/android/src/md5be647e183c0bcd7b2b5b53dc44b7300a/GetInfo.java
@@ -0,0 +1,56 @@
+package md5be647e183c0bcd7b2b5b53dc44b7300a;
+
+
+public class GetInfo
+	extends android.app.DialogFragment
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreateView:(Landroid/view/LayoutInflater;Landroid/view/ViewGroup;Landroid/os/Bundle;)Landroid/view/View;:GetOnCreateView_Landroid_view_LayoutInflater_Landroid_view_ViewGroup_Landroid_os_Bundle_Handler\n" +
+			"n_onActivityCreated:(Landroid/os/Bundle;)V:GetOnActivityCreated_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("Client_SignalR.GetInfo, Client_SignalR", GetInfo.class, __md_methods);
+	}
+
+
+	public GetInfo ()
+	{
+		super ();
+		if (getClass () == GetInfo.class)
+			mono.android.TypeManager.Activate ("Client_SignalR.GetInfo, Client_SignalR", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public android.view.View onCreateView (android.view.LayoutInflater p0, android.view.ViewGroup p1, android.os.Bundle p2)
+	{
+		return n_onCreateView (p0, p1, p2);
+	}
+
+	private native android.view.View n_onCreateView (android.view.LayoutInflater p0, android.view.ViewGroup p1, android.os.Bundle p2);
+
+
+	public void onActivityCreated (android.os.Bundle p0)
+	{
+		n_onActivityCreated (p0);
+	}
+
+	private native void n_onActivityCreated (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ SignalR_Server/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalR_Server.Startup))]
+
+namespace SignalR_Server
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
